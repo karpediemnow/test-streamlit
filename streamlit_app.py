56814b9667f6c160fe3b14baf254610c9bbe0aad
Gianpaolo Iannetta <g.iannetta@teamsysten.com>
2023-06-23 16:33:39 +0200

    secret

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import streamlit_authenticator as stauth
+#import streamlit_authenticator as stauth
 
 #https://blog.streamlit.io/streamlit-quests-getting-started-with-streamlit/#:~:text=Streamlit%20Quests%3A%20Getting%20started%20with%20Streamlit%201%20Two,Streamlit%20apps%20in%20the%20Gallery%20...%20Altri%20elementi
 
@@ -16,10 +16,16 @@ def main():
       st.write("Welcome to Home Page")
 
     with tabConfig:
-       placeholder = st.empty()
-       psw = placeholder.text_input("Password to hash:")
-       hashed_passwords = stauth.Hasher([psw]).generate()
-       st.write("\""+hashed_passwords+"\"")          
+       
+       # Everything is accessible via the st.secrets dict:
+       st.write("DB username:", st.secrets["db_username"])
+       st.write("DB password:", st.secrets["db_password"])
+       st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
+
+       #placeholder = st.empty()
+       #psw = placeholder.text_input("Password to hash:")
+       #hashed_passwords = stauth.Hasher([psw]).generate()
+       #st.write("\""+hashed_passwords+"\"")          
 
 
 
